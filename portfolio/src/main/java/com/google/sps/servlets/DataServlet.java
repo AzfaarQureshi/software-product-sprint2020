793da9c44f7948288e0a5a31a4d291f429d802ac
Azfaar Qureshi <ar2qures@uwaterloo.ca>
2020-06-14 09:29:45 -0700

    fetching hardcoded comments from servlet

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -13,20 +13,47 @@
 // limitations under the License.
 
 package com.google.sps.servlets;
-
+import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+  private ArrayList<String> messages;
+
+  @Override
+  public void init() {
+    messages = new ArrayList<>();
+    messages.add(
+        "Azfaar Qureshi");
+    messages.add("They told me computers could only do arithmetic. - Grace Hopper");
+    messages.add("A ship in port is safe, but that's not what ships are built for. - Grace Hopper");
+    messages.add("It is much easier to apologise than it is to get permission. - Grace Hopper");
+    messages.add("If you can't give me poetry, can't you give me poetical science? - Ada Lovelace");
+    messages.add("I am in a charming state of confusion. - Ada Lovelace");
+    messages.add(
+        "The Analytical Engine weaves algebraic patterns, "
+            + "just as the Jacquard loom weaves flowers and leaves. - Ada Lovelace");
+    messages.add(
+        "Sometimes it is the people no one can imagine anything of "
+            + "who do the things no one can imagine. - Alan Turing");
+    messages.add("Those who can imagine anything, can create the impossible. - Alan Turing");
+  }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Azfaar Qureshi</h1>");
+    response.getWriter().println(convertToJsonUsingGson(messages));
+  }
+
+  private String convertToJsonUsingGson(ArrayList message) {
+    Gson gson = new Gson();
+    String json = gson.toJson(message);
+    return json;
   }
 }
